1290a75044ea142cc3c203fb89e0a7c761184e67
Nic <nico.nguyen22@gmail.com>
2022-06-10 21:57:40 -0500

    added softmax, d_softmax, one_hot to utils.py

--- utils.py
+++ utils.py
@@ -16,4 +16,27 @@ def d_relu(x):
   return 1 if x > 0 else 0
 
 def mean_squared_error(A, B):
-  return (np.square(A - B)).mean(axis=1)
+  return np.square(np.subtract(A,  B)).mean()
+
+# TODO: np.exp and np.sum can be trivially combined into one loop as an optimization
+def softmax(x):
+  exponentiated = np.exp(x)
+  sum = np.sum(exponentiated)
+
+  return [exp / sum for exp in exponentiated]
+
+def d_softmax(x):
+  update_vectors = []
+  for i in range(len(x)):
+    updates_from_this_neuron = []
+    for j in range(len(x)):
+      # softmax magic
+      update_val = x[i] * (1 - x[j]) if i == j else -x[i] * x[j]
+      updates_from_this_neuron.append(update_val)
+    update_vectors.append(updates_from_this_neuron)
+
+  # sum all the updates and return the derivative vector
+  return np.sum(update_vectors, axis=0)
+
+def one_hot(value, total_values):
+  return [1 if i == value else 0 for i in range(total_values)]
