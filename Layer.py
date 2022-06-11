af0346445df928b1e3eb5fcd57967823de923572
Nic <nico.nguyen22@gmail.com>
2022-06-10 20:25:23 -0500

    optimized and typed Layer.py

--- Layer.py
+++ Layer.py
@@ -2,10 +2,13 @@ import numpy as np
 import utils
 
 class Layer:
-  def __init__(self, num_neurons, num_inputs, activation):
+  def __init__(self, num_neurons: int, num_inputs: int, activation: str):
     self.weights = np.random.randn(num_neurons, num_inputs)
     self.biases = np.random.randn(num_neurons)
-    self.current_inputs = []
+
+    # used to simplify back-propagation
+    self.current_input = None
+    self.pre_activation_result = None
 
     # default activation is none
     self.activation = utils.identity
@@ -18,25 +21,27 @@ class Layer:
       self.activation = utils.relu
       self.d_activation = utils.d_relu
 
-  def forward_pass(self, inputs):
-    self.current_inputs = inputs
-    return self.activation(np.dot(self.weights, inputs) + self.biases)
+  def forward_pass(self, _input: list):
+    self.current_input = _input
+    self.pre_activation_result = np.dot(self.weights, _input) + self.biases
+
+    return self.activation(self.pre_activation_result)
 
   # returns new "running gradient"
   def back_propagate(self, running_gradient):
     # calculate gradient for activation function
-    activation_gradient = self.d_activation(self.current_inputs)
+    activation_gradient = self.d_activation(self.pre_activation_result)
 
     # tack activation gradient onto running gradient
     new_running_gradient = np.dot(running_gradient, activation_gradient)
 
     # calculate gradients for weights and biases
-    d_weights = np.dot(new_running_gradient, self.current_inputs)
+    d_weights = np.dot(new_running_gradient, self.current_input)
     d_biases = new_running_gradient
 
     # adjust weights and biases
-    self.weights = np.subtract(self.weights, d_weights)
-    self.biases = np.subtract(self.biases, d_biases)
+    self.weights = self.weights - d_weights
+    self.biases = self.biases - d_biases
 
     # tack this layer's weights onto running gradient and return
     return np.dot(new_running_gradient, self.weights)
